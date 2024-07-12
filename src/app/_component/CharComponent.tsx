b3c707fd62e79129d3dc2e0864560b8a53ccf41e
JHG <jhg990508@gmail.com>
2024-07-12 16:15:55 +0900

    캐릭터 컴포넌트 css

--- src/app/_component/CharComponent.tsx
+++ src/app/_component/CharComponent.tsx
@@ -1,25 +1,29 @@
 import { Char } from "@/type/char";
 import Image from "next/image";
-
+import classes from "./charComponent.module.css";
 export default function CharComponent({ data }: { data: Char }) {
   if (!data) return null;
   const { character_name, character_class, character_gender, character_guild_name, character_image, character_level } =
     data;
+
   return (
-    <div>
+    <div className={classes.container}>
       <div>
-        <Image src={character_image} alt="" width={100} height={100} />
+        <Image src={character_image} alt="userMainCharacterImage" width={100} height={100} />
       </div>
-      <div>
+      <div className={classes.charInfo}>
         <div>
-          <p>
-            {character_name}({character_gender})/ {character_class}
-          </p>
+          <p>닉네임 : {character_name}</p>
         </div>
         <div>
-          <p>
-            {character_level} {character_guild_name}
-          </p>
+          <p>직업 : {character_class}</p>
+        </div>
+        <div>
+          <p> 레벨 : {character_level} </p>
+        </div>
+
+        <div>
+          <p>길드 : {character_guild_name}</p>
         </div>
       </div>
     </div>
