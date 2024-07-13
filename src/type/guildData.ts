a980163928c156a90ac8f7e404ba7ae5bed0b5a5
JHG <jhg990508@gmail.com>
2024-07-13 16:12:43 +0900

    css, 타입추가

---
+++ src/type/guildData.ts
@@ -0,0 +1,26 @@
+type GuildData = {
+  date: string | null;
+  guild_fame: number;
+  guild_level: number;
+  guild_master_name: string;
+  guild_member: string[];
+  guild_member_count: number;
+  guild_name: string;
+  guild_noblesse_skill: {
+    skill_description: string;
+    skill_effect: string;
+    skill_icon: string;
+    skill_level: number;
+    skill_name: string;
+  }[];
+  guild_point: number;
+  guild_skill: {
+    skill_description: string;
+    skill_effect: string;
+    skill_icon: string;
+    skill_level: number;
+    skill_name: string;
+  }[];
+  world_name: string;
+  currentNoblePoint?: number;
+};
