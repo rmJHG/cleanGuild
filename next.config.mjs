a568f9d37c4e0a6dc43e66262512d3f9c66092bb
JHG <jhg990508@gmail.com>
2024-07-26 11:27:59 +0900

    제거

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 const nextConfig = {
   images: {
@@ -12,20 +10,6 @@ const nextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
   },
-};
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /.s?css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-    ],
-  },
-  optimization: {
-    minimizer: [`...`, new CssMinimizerPlugin()],
-  },
-  plugins: [new MiniCssExtractPlugin()],
 };
 
 export default nextConfig;
