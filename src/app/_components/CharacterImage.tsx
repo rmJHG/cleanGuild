46b7643779069b47589bb9cb01ad7e3a9d772449
hangyu <jhg990508@gmail.com>
2025-01-14 01:25:26 +0900

    feat: 개인정보취급방침 추가, footer client component로 변경

--- src/app/_components/CharacterImage.tsx
+++ src/app/_components/CharacterImage.tsx
@@ -1,15 +1,22 @@
-import Image from "next/image";
-import classes from "./styles/CharacterImage.module.css";
-import { auth } from "@/auth";
+'use client';
+import Image from 'next/image';
+import classes from './styles/CharacterImage.module.css';
+import { useSession } from 'next-auth/react';
 
-export default async function CharacterImage() {
-  const session = await auth();
+export default function CharacterImage() {
+  const { data: session } = useSession();
   if (!session) return null;
   const { handsData } = session.user;
   if (!handsData) return null;
   return handsData!.character_image ? (
     <div className={classes.imageWrapeer}>
-      <Image src={handsData!.character_image} alt="user_main_character_image" priority width={80} height={80} />
+      <Image
+        src={handsData!.character_image}
+        alt="user_main_character_image"
+        priority
+        width={80}
+        height={80}
+      />
     </div>
   ) : null;
 }
--- src/app/_components/layout/Footer.tsx
+++ src/app/_components/layout/Footer.tsx
@@ -1,9 +1,34 @@
+'use client';
+import Link from 'next/link';
 import CharacterImage from '../CharacterImage';
 import classes from './styles/footer.module.css';
+
+import { MdOutlineEmail } from 'react-icons/md';
+import { successModal } from '@/app/_lib/successModal';
+
 export default function Footer() {
+  const email = 'service@maplegremio.com';
+  const handleEmail = () => {
+    navigator.clipboard
+      .writeText(email)
+      .then(() => {
+        successModal('이메일 주소가 복사되었습니다.', 1000);
+      })
+      .catch((err) => {
+        console.error('이메일 주소 복사에 실패했습니다.', err);
+      });
+  };
+
   return (
     <div className={classes.footerWrapper}>
-      <p>service@maplegremio.com</p>
+      <div>
+        <Link href={'/privacy'} className={classes.privacy}>
+          개인정보 처리방침
+        </Link>
+        <div className={classes.gap}></div>
+        <p>문의 : </p>
+        <MdOutlineEmail size={16} color="white" onClick={handleEmail} />
+      </div>
       <p>
         Data based on <span style={{ fontFamily: 'SBAggroB' }}>NEXON OPEN API</span>
       </p>
