1b455e7e7d78e5b857614fbfafe717d06f1d1573
hangyu <jhg990508@gmail.com>
2025-02-26 22:55:52 +0900

    feat: 게시글 조회 실패 예외처리

--- src/app/(headerPage)/find/[server]/[postId]/page.tsx
+++ src/app/(headerPage)/find/[server]/[postId]/page.tsx
@@ -1,3 +1,4 @@
+import { redirect } from 'next/navigation';
 import Post from './_component/Post';
 import { GuildPostData } from '@/types/guildPostData';
 
@@ -29,5 +30,8 @@ export default async function Page({ params }: Props) {
   );
   const data: GuildPostData = await res.json();
 
+  if (data.message === '게시글 데이터를 가져오는데 실패했습니다.') {
+    return redirect('/');
+  }
   return <Post data={data} />;
 }
